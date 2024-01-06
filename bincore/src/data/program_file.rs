e44669a6659b45678eda85c4e241b5985685c7f4
Eric <jhonjhonjumaday@gmail.com>
2024-01-06 20:17:27 +0800

    removed functions as it's no longer needed

--- bincore/src/data/program_file.rs
+++ bincore/src/data/program_file.rs
@@ -1,12 +1,30 @@
+use std::collections::HashMap;
+use std::rc::Rc;
 use serde::{Deserialize, Serialize};
-use crate::data::function::{Function};
-use crate::data::object::ObjectBuilder;
+use crate::data::object::{ObjectBuilder, ObjectDescriptor};
 use crate::executable::runnable::Instruction;
 
+#[derive(Debug, Clone, Deserialize, Serialize)]
+pub struct ObjectBuilderLayout {
+    pub fields: Vec<String>,
+    pub members: HashMap<String, usize>,
+    pub name: String
+}
+
+impl ObjectBuilderLayout {
+    pub fn into_builder(self) -> ObjectBuilder {
+        ObjectBuilder {
+            name: self.name.clone(),
+            descriptor: Rc::new(ObjectDescriptor {
+                name: self.name.clone(),
+                members: self.members
+            })
+        }
+    }
+}
 
 #[derive(Debug, Clone, Deserialize, Serialize)]
 pub struct Program {
     pub instructions: Vec<Instruction>,
-    pub functions: Vec<Function>,
-    pub object_builder: Vec<ObjectBuilder>
+    pub object_builder: Vec<ObjectBuilderLayout>
 }
