7b14d21a34bfba6d3a065d00616974701147307b
CunningBard <cunningbard@gmail.com>
2024-01-30 13:52:31 +0800

    docs: added warning for dev when running perf reader, ALWAYS PUT IT BACK TO 4

--- makesvg_linux.sh
+++ makesvg_linux.sh
@@ -1,3 +1,5 @@
+echo "DO NOT STOP MID PROCESS, IF YOU DO, RUN THE PROCESS BELOW"
+echo "echo 4 | sudo tee /proc/sys/kernel/perf_event_paranoid"
 echo -1 | sudo tee /proc/sys/kernel/perf_event_paranoid
 cargo flamegraph --package binrunner
 echo 4 | sudo tee /proc/sys/kernel/perf_event_paranoid
